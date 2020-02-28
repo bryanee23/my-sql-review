741e52ded7b70a1743e6449deb1461662c4d9090
Bryan Evangelista <bryanevangelista@Bryans-MacBook-Pro.local>
2020-02-27 19:47:33 -0800

    completed intial task

--- database/index.js
+++ database/index.js
@@ -1,1 +1,19 @@
-// TODO
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat',
+})
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to server')
+  };
+}
+module.exports = db;
+//CREATE DATABASE ball;
+//show database
--- server/controllers.js
+++ server/controllers.js
@@ -1,12 +1,26 @@
+const { readdAll, addItem } = require('../database/models.js');
 
 
 const controllers = {
-  getItems: () => {
-    // TODO
+  getItems: (req, res) => {
+    readdAll((err) => {
+      if (err) {
+        res.status(400);
+      } else {
+        res.status(200).send(result);
+      }
+    })
     console.log('We got it');
   },
-  postItems: () => {
-    // TODO
+  //called by router
+  postItems: (req, res) => {
+    addItem(req.body, (err) => {
+      if (err) {
+        res.status(400);
+      } else {
+        res.status(200).send('item added');
+      }
+    })
     console.log('This is your post request speaking')
   }
 }
