56210047ab9b28582af01d99139b95e47ba35d3c
Hack Reactor Student <student@hackreactor.com>
2020-02-29 13:39:19 -0800

    refactoring sql review

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,2 @@
-DROP DATABASE IF EXISTS ball;
-CREATE DATABASE ball;
-
-USE ball
-
-CREATE TABLE tennis (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  item_name VARCHAR(255) NOT NULL,
-  amount INT NOT NULL
-);
+-- how to clear db (⌥ + CMD + L)
+-- mysql -u student -p  student
--- server/controllers.js
+++ server/controllers.js
@@ -1,26 +1,12 @@
-const { readdAll, addItem } = require('../database/models.js');
 
 
 const controllers = {
-  getItems: (req, res) => {
-    readdAll((err) => {
-      if (err) {
-        res.status(400);
-      } else {
-        res.status(200).send(result);
-      }
-    })
+  getItems: () => {
+    // TODO
     console.log('We got it');
   },
-  //called by router
-  postItems: (req, res) => {
-    addItem(req.body, (err) => {
-      if (err) {
-        res.status(400);
-      } else {
-        res.status(200).send('item added');
-      }
-    })
+  postItems: () => {
+    // TODO
     console.log('This is your post request speaking')
   }
 }
